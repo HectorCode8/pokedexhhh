d0bfb8f74578fda78736a9e3549e3ec38612c13d
Hector Haro H <hector8.haro@gmail.com>
2022-04-23 00:55:52 -0500

    add more logic and finish the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect} from 'react';
-
+import Pokemon from './components/pokemon';
 
 function App() {
 
@@ -11,6 +11,20 @@ function App() {
     const data = await res.json(); 
 
     setLoadMore(data.next);
+
+    function createPokemonObject (result) {
+      result.forEach( async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json();
+
+        setAllPokemons(currentList => [...currentList, data])
+        
+        
+      })
+    }
+    
+    createPokemonObject(data.results);
+    await console.log(allPokemons)
   }
 
   useEffect(() => {
@@ -22,9 +36,17 @@ function App() {
       <h1>Pokemon</h1>
       <div className="pokemon-container">
         <div className="all-container">
-
+          {allPokemons.map((pokemon, index) => 
+            <Pokemon 
+              id={pokemon.id}
+              name={pokemon.name}
+              image={pokemon.sprites.other.dream_world.front_default}
+              type={pokemon.types[0].type.name}
+              key={index}
+              />  
+          )}
         </div>
-        <button className="load-more">Load More</button>
+        <button className="load-more" onClick={() => getAllPokemons()}>Load More</button>
       </div>
     </div>
   );
